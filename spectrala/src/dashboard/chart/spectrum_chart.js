fa00b51c204b423d9ed80cc608446759eec51d38
Ian Fowler <ian@spidr.design>
2020-10-25 12:57:17 -0500

    Messed with chart margins

--- spectrala/src/dashboard/chart/spectrum_chart.js
+++ spectrala/src/dashboard/chart/spectrum_chart.js
@@ -6,16 +6,11 @@ import spectrum from './spectrum_chart.png';
 import {ResponsiveLine} from '@nivo/line';
 import {data} from './sample_data';
 
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
 const MyResponsiveLine = () => {
     return (
         <ResponsiveLine
         data={data}
-        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+        margin={{ top: 15, right: 15, bottom: 50, left: 60 }}
         xScale={{ type: 'linear', min: 'auto', max: 'auto'  }}
         yScale={{ type: 'linear', min: '0', max: '100' }}
         yFormat=" >-.2f"
@@ -23,14 +18,6 @@ const MyResponsiveLine = () => {
         axisTop={null}
         axisRight={null}
         axisBottom={{
-            tickValues: [
-                382.2,
-                443.4,
-                504.7,
-                566.0,
-                627.3,
-                688.6
-            ],
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
@@ -40,23 +27,15 @@ const MyResponsiveLine = () => {
             legendPosition: 'middle'
         }}
         axisLeft={{
-            tickValues: [
-                0,
-                20,
-                40,
-                60,
-                80,
-                100
-            ],
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            format: '.2s',
+            format: '.0f',
             legend: 'intensity',
             legendOffset: -40,
             legendPosition: 'middle'
         }}
-        enableGridX={false}
+        enableGridX={true}
         colors={{ scheme: 'spectral' }}
         lineWidth={1}
         pointSize={4}
@@ -68,8 +47,6 @@ const MyResponsiveLine = () => {
         areaBaselineValue={0}
         areaOpacity={0.1}
         useMesh={true}
-        gridXValues={[ 300, 600, 900 ]}
-        gridYValues={[ 0, 500, 1000, 1500, 2000, 2500 ]}
         legends={[]}
     />
 )}
