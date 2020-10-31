308710f9632ee7eb244738f5f656f49e90dc6592
Josh Hejna <josh.hejna@gmail.com>
2020-10-30 19:45:06 -0700

    WIP

--- spectrala/src/dashboard/video/camera_implementations/webcam.js
+++ spectrala/src/dashboard/video/camera_implementations/webcam.js
@@ -2,13 +2,18 @@ import React from 'react';
 import placeholder from './rainbow_placeholder.jpg';
 import PropTypes from 'prop-types';
 
-
 export default class WebcamView extends React.Component {
     render() {
-        return <img src={placeholder} style={{height: this.props.height}} alt={"Dummy spectral graph"}/>
+        return (
+            <img
+                src={placeholder}
+                style={{ height: this.props.height }}
+                alt={'Dummy spectral graph'}
+            />
+        );
     }
 }
 
 WebcamView.propTypes = {
-  height: PropTypes.number
-}
+    height: PropTypes.number,
+};
