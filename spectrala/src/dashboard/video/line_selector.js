384ec9b958d2a3ca3da1da3e901bb4740e1563d2
Josh Hejna <josh.hejna@gmail.com>
2020-11-06 20:22:22 -0800

    Fix indentation in line_selector.js

--- spectrala/src/dashboard/video/line_selector.js
+++ spectrala/src/dashboard/video/line_selector.js
@@ -1,70 +1,70 @@
-import React, { useState } from "react";
-import { Col, InputGroup, FormControl } from "react-bootstrap";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import { Col, InputGroup, FormControl } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 function invalidToNull(x) {
-  if (isNaN(x)) return null;
-  if (x < 0 || x > 1) return null;
-  return x;
+    if (isNaN(x)) return null;
+    if (x < 0 || x > 1) return null;
+    return x;
 }
 
 export default function LineSelector({ onChange }) {
-  const [calibCoords, setCalibCoords] = useState({
-    lowX: null,
-    lowY: null,
-    highX: null,
-    highY: null,
-  });
+    const [calibCoords, setCalibCoords] = useState({
+        lowX: null,
+        lowY: null,
+        highX: null,
+        highY: null,
+    });
 
-  const updateCalibCoords = (newval) => {
-    setCalibCoords(newval);
-    onChange(newval);
-  };
+    const updateCalibCoords = (newval) => {
+        setCalibCoords(newval);
+        onChange(newval);
+    };
 
-  const createFormControl = (nm, stateTarget) => {
+    const createFormControl = (nm, stateTarget) => {
+        return (
+            <FormControl
+                placeholder={nm + ' pct.'}
+                aria-label={nm + ' percentage'}
+                isInvalid={calibCoords[stateTarget] === null}
+                onChange={(event) => {
+                    const value = invalidToNull(event.target.value / 100);
+                    updateCalibCoords({ ...calibCoords, [stateTarget]: value });
+                }}
+            />
+        );
+    };
+
     return (
-      <FormControl
-        placeholder={nm + " pct."}
-        aria-label={nm + " percentage"}
-        isInvalid={calibCoords[stateTarget] === null}
-        onChange={(event) => {
-          const value = invalidToNull(event.target.value / 100);
-          updateCalibCoords({ ...calibCoords, [stateTarget]: value });
-        }}
-      />
+        <>
+            <Col>
+                <InputGroup>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>Low Energy</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    {createFormControl('X', 'lowX')}
+                    {createFormControl('Y', 'lowY')}
+                </InputGroup>
+            </Col>
+            <Col>
+                <InputGroup>
+                    <InputGroup.Prepend>
+                        <InputGroup.Text>High Energy</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    {createFormControl('X', 'highX')}
+                    {createFormControl('Y', 'highY')}
+                </InputGroup>
+            </Col>
+        </>
     );
-  };
-
-  return (
-    <>
-      <Col>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Low Energy</InputGroup.Text>
-          </InputGroup.Prepend>
-          {createFormControl("X", "lowX")}
-          {createFormControl("Y", "lowY")}
-        </InputGroup>
-      </Col>
-      <Col>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>High Energy</InputGroup.Text>
-          </InputGroup.Prepend>
-          {createFormControl("X", "highX")}
-          {createFormControl("Y", "highY")}
-        </InputGroup>
-      </Col>
-    </>
-  );
 }
 
 LineSelector.propTypes = {
-  onChange: PropTypes.func,
+    onChange: PropTypes.func,
 };
 
 LineSelector.defaultProps = {
-  onChange: () => {
-    console.warn("Unimplemented onChange for LineSelector");
-  },
+    onChange: () => {
+        console.warn('Unimplemented onChange for LineSelector');
+    },
 };
