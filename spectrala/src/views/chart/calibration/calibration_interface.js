52847ba8e32a7ee71ec6512643d01824007a2eb2
Ian Fowler <ian@spidr.design>
2020-11-17 11:13:43 -0600

    powerful functions the likes of which have never been seen before

--- spectrala/src/views/chart/calibration/calibration_interface.js
+++ spectrala/src/views/chart/calibration/calibration_interface.js
@@ -3,37 +3,9 @@ import { Col, Card, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import CalibrationPointsControl from './points_control';
 import CalibrationLine from './calibration_line';
-import CalibrationPoints from '../../../reducers/calibration/calibration_points';
-import CalibrationPoint from '../../../reducers/calibration/calibration_point';
 
-export default class SpectrumChart extends React.Component {
-    state = {
-        calibrationPoints: new CalibrationPoints(
-            [
-                new CalibrationPoint(303, 0.3670886075949367, false),
-                new CalibrationPoint(421, null, true),
-            ],
-            () => this.onCalibrationPointChange()
-        ),
-    };
-
-    componentDidMount() {
-        this._ismounted = true;
-    }
-
-    componentWillUnmount() {
-        this._ismounted = false;
-    }
-
-    onCalibrationPointChange = () => {
-        // The value is already changed, just need to trigger the refresh.
-        // Force refresh is not reccomended, so this will do the trick.
-        if (this._ismounted) {
-            this.setState({ calibrationPoints: this.state.calibrationPoints });
-        }
-    };
-
-    getHeader = () => {
+export default function SpectrumChart({ height }) {
+    function getHeader() {
         return (
             <Card.Header
                 as="h5"
@@ -48,34 +20,25 @@ export default class SpectrumChart extends React.Component {
                 Calibration
             </Card.Header>
         );
-    };
-
-    render() {
-        return (
-            <Row style={{ justifyContent: 'center', display: 'flex' }}>
-                <Col xs lg={8}>
-                    <Card>
-                        {this.getHeader()}
-                        <div style={{ height: this.props.height }}>
-                            <CalibrationLine
-                                cameraFeed={this.props.cameraFeed}
-                                calibrationPoints={this.state.calibrationPoints}
-                            />
-                        </div>
-                    </Card>
-                </Col>
-                <Col xs lg={3}>
-                    <CalibrationPointsControl
-                        height={this.props.height}
-                        calibrationPoints={this.state.calibrationPoints}
-                    />
-                </Col>
-            </Row>
-        );
     }
+
+    return (
+        <Row style={{ justifyContent: 'center', display: 'flex' }}>
+            <Col xs lg={8}>
+                <Card>
+                    {getHeader()}
+                    <div style={{ height: height }}>
+                        <CalibrationLine data={null} />
+                    </div>
+                </Card>
+            </Col>
+            <Col xs lg={3}>
+                <CalibrationPointsControl height={height} />
+            </Col>
+        </Row>
+    );
 }
 
 SpectrumChart.propTypes = {
     height: PropTypes.number,
-    cameraFeed: PropTypes.object,
 };
