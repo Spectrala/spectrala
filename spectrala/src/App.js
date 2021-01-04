95130433b532769466d2f951d0a0a8ac48c9d0b7
Ian Fowler <ian@spidr.design>
2021-01-03 20:30:57 -0600

    Beforeunload

--- spectrala/src/App.js
+++ spectrala/src/App.js
@@ -4,16 +4,16 @@ import Dashboard from './views/dashboard.js';
 
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-// import { Beforeunload } from 'react-beforeunload';
+import { Beforeunload } from 'react-beforeunload';
 
 export default class App extends Component {
     render() {
         return (
-            // <Beforeunload onBeforeunload={(event) => event.preventDefault()}>
+            <Beforeunload onBeforeunload={(event) => event.preventDefault()}>
                 <DndProvider backend={HTML5Backend}>
                     <Dashboard />
                 </DndProvider>
-            // </Beforeunload>
+            </Beforeunload>
         );
     }
 }
