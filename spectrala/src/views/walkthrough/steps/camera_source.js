a6a78d316af6a7961910f0e947bee57cf983ef65
Ian Fowler <ian@spidr.design>
2021-01-06 22:22:33 -0600

    isLocal as part of walkthrough

--- spectrala/src/views/walkthrough/steps/camera_source.js
+++ spectrala/src/views/walkthrough/steps/camera_source.js
@@ -1,65 +1,71 @@
 import React from 'react';
-
+import { isLocal } from '../../video/source_select';
 export default (
     <div>
         Establish a visual of the spectrum inside the spectrometer.
-        <details>
-            <summary>
-                Stream from an iPhone or Android using the app DroidCam
-            </summary>
-            <ol type="1">
-                <li>
-                    Install DroidCam on your device (
-                    <a
-                        href="https://apps.apple.com/us/app/droidcam/id1510258102"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        iOS
-                    </a>
-                    ) (
-                    <a
-                        href="https://play.google.com/store/apps/details?id=com.dev47apps.droidcam&hl=en_US&gl=US"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Android
-                    </a>
-                    ).
-                </li>
-                <li>
-                    In the source selection menu on the left side of the screen,
-                    select the leftmost icon that looks like a smartphone.
-                </li>
-                <li>
-                    Ensure your smartphone and computer are connected to the
-                    same local network (Both are using Wi-Fi with the same
-                    name).
-                </li>
-                <li>
-                    Input the IP and Port displayed on the screen in the
-                    DroidCam app into Spectrala under the source select buttons.
-                </li>
-            </ol>
-        </details>
-        <details>
-            <summary>Any other stream on your local network</summary>
-            <ol type="1">
-                <li>
-                    In the source selection menu on the left side of the screen,
-                    select the icon second to the left that looks like a
-                    broadcast pin.
-                </li>
-                <li>
-                    Ensure your camera stream and computer are connected to the
-                    same local network.
-                </li>
-                <li>
-                    Input the stream URL into Spectrala under the source select
-                    buttons.
-                </li>
-            </ol>
-        </details>
+        {isLocal && (
+            <>
+                <details>
+                    <summary>
+                        Stream from an iPhone or Android using the app DroidCam
+                    </summary>
+                    <ol type="1">
+                        <li>
+                            Install DroidCam on your device (
+                            <a
+                                href="https://apps.apple.com/us/app/droidcam/id1510258102"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                iOS
+                            </a>
+                            ) (
+                            <a
+                                href="https://play.google.com/store/apps/details?id=com.dev47apps.droidcam&hl=en_US&gl=US"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Android
+                            </a>
+                            ).
+                        </li>
+                        <li>
+                            In the source selection menu on the left side of the
+                            screen, select the leftmost icon that looks like a
+                            smartphone.
+                        </li>
+                        <li>
+                            Ensure your smartphone and computer are connected to
+                            the same local network (Both are using Wi-Fi with
+                            the same name).
+                        </li>
+                        <li>
+                            Input the IP and Port displayed on the screen in the
+                            DroidCam app into Spectrala under the source select
+                            buttons.
+                        </li>
+                    </ol>
+                </details>
+                <details>
+                    <summary>Any other stream on your local network</summary>
+                    <ol type="1">
+                        <li>
+                            In the source selection menu on the left side of the
+                            screen, select the icon second to the left that
+                            looks like a broadcast pin.
+                        </li>
+                        <li>
+                            Ensure your camera stream and computer are connected
+                            to the same local network.
+                        </li>
+                        <li>
+                            Input the stream URL into Spectrala under the source
+                            select buttons.
+                        </li>
+                    </ol>
+                </details>
+            </>
+        )}
         <details>
             <summary>Stream from a webcam</summary>
             <ol type="1">
