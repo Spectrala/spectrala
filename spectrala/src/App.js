52121783cd8176ed0ab9c856c509e94ed7eabc32
Ian Fowler <ian@spidr.design>
2021-01-15 15:27:20 -0600

    Added alert for other browsers

--- spectrala/src/App.js
+++ spectrala/src/App.js
@@ -7,6 +7,18 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Beforeunload } from 'react-beforeunload';
 
 export default class App extends Component {
+    componentDidMount() {
+        const sUsrAg = navigator.userAgent;
+        // TODO: Make this much less hacky. 
+        // TODO: Support all browsers.
+        if (sUsrAg.indexOf('Chrome') === -1) {
+            window.alert(
+                'Please use Chrome to have the best experience spectrala.net. \
+Some features may not be availible in this browser.'
+            );
+        }
+    }
+
     render() {
         return (
             <Beforeunload onBeforeunload={(event) => event.preventDefault()}>
